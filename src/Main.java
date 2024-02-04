18dbf6ed6198b0ac12bae26d16baf60548100e04
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-02-04 19:13:07 -0300

    finished exercise: 4)Crie uma listagem de clientes utilizando variáveis primitivas

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    static void printLine(){
+        System.out.println("--------------------------------");
+    }
+
+    public static void main(String[] args) {
+
+        //4) Crie uma listagem de clientes utilizando variáveis primitivas
+        String[] nome = {"Fulano", "Sicrano"}, idade = {"1980", "2000"};
+
+        System.out.println("Listagem dos Clientes");
+        for (int i = 0; i < nome.length; i++){
+            printLine();
+            System.out.println("Nome: " + nome[i]);
+            System.out.println("Data de Nascimento: " + idade[i]);
         }
+
+
     }
 }
