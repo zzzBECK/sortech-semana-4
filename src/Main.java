ede3be48002a3f77addd68298775697d6a957783
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-02-05 00:23:27 -0300

    finished exercise: 6) Alterar a listagem dos clientes para utilizar o nome dos clientes em variáveis do tipo String

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //4) Crie uma listagem de clientes utilizando variáveis primitivas
+        // 4) Crie uma listagem de clientes utilizando variáveis primitivas
         String[] nome = {"Fulano", "Sicrano"}, idade = {"1980", "2000"};
 
         System.out.println("Listagem dos Clientes");
@@ -19,7 +19,7 @@ public class Main {
         printLine();
         printLine();
 
-        //5) Crie uma listagem de produtos utilizando variáves primitivas
+        // 5) Crie uma listagem de produtos utilizando variáves primitivas
         String[] produtos = {"Livro", "Celular"};
         double[] preco = {49.95, 1470.76};
 
@@ -30,5 +30,8 @@ public class Main {
             System.out.println("Preço: " + preco[i]);
         }
 
+
+        // 6) Alterar a listagem dos clientes para utilizar o nome dos clientes em variáveis do tipo String
+        // Já foi feito
     }
 }
